9adcf76dd77c12bd10c62e274d8187ddd6610485
Stanislav Rubint <stanislav@rubint.sk>
2021-08-13 08:43:12 +0200

    working project

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ColourLedBatteryMeter.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ColourLedBatteryMeter.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=colourledbatterymeter.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/colourledbatterymeter.x.tar
